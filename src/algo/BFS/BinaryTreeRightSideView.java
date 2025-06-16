4272fa37cbb9e0c9230de67102b21b238ff07bd4
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2025-06-16 07:47:38 -0400

    Added solution for right side view of a binary tree and merge K sorted list.

--- src/algo/BFS/BinaryTreeRightSideView.java
+++ src/algo/BFS/BinaryTreeRightSideView.java
@@ -1,4 +1,43 @@
 package algo.BFS;
 
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        Queue<TreeNode> currentLevelQueue = new LinkedList<>();
+        Queue<TreeNode> nextLevelQueue = new LinkedList<>();
+
+        nextLevelQueue.add(root);
+        List<Integer> result = new ArrayList<>();
+
+        TreeNode node  = null;
+
+        while(!nextLevelQueue.isEmpty()) {
+            currentLevelQueue = nextLevelQueue;
+
+            nextLevelQueue = new LinkedList<>();
+
+            while(!currentLevelQueue.isEmpty()) {
+                node = currentLevelQueue.poll();
+
+                if (node.left != null) nextLevelQueue.offer(node.left);
+                if (node.right != null) nextLevelQueue.offer(node.right);
+            }
+            result.add(node.val);
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        BinaryTreeRightSideView obj  = new BinaryTreeRightSideView();
+        List<Integer> res = obj.rightSideView(new TreeNode(1, new TreeNode(2, new TreeNode(6, null, new TreeNode(7)), null), new TreeNode(3, new TreeNode(4), new TreeNode(5))));
+        System.out.println(res.toString());
+    }
 }
--- src/algo/DivideAndConquer/MergeKSortedLists.java
+++ src/algo/DivideAndConquer/MergeKSortedLists.java
@@ -1,4 +1,91 @@
 package algo.DivideAndConquer;
 
+import common.ListNode;
+
+/**
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ *
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ *
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ * Explanation: The linked-lists are:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * merging them into one sorted list:
+ * 1->1->2->3->4->4->5->6
+ *
+ * The idea is to merge every 2 lists together and repeat until there is one left
+ * [list 0, list 1, list 2, list 3, list 4]
+ * merge(list 0, list 1); -> list 0
+ * merge(list 2, list 3); -> list 2
+ *
+ * 2nd Iteration:
+ * merge(list 0, list 2) -> list 0
+ *
+ * 3rd Iteration
+ * merge(list 0, list 4) -> list 0
+ *
+ * We need to have an interval, which will help to guide which lists to merge
+ *
+ */
 public class MergeKSortedLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        int numberOfNodes = lists.length;
+        int interval = 1;
+
+        while(interval < numberOfNodes) {
+            for(int i=0; i< numberOfNodes - interval; i = i+interval*2) {
+                lists[i]= merge(lists[i], lists[i+interval]);
+            }
+            interval = interval * 2; // Pairwise merging
+        }
+
+        return numberOfNodes > 0 ? lists[0]:null;
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail= tail.next;
+        }
+
+        tail.next = (l1!= null) ? l1: l2;
+        return head.next;
+    }
+
+    public void printLinkedList(ListNode head) {
+        ListNode current = head;
+
+        while(current.next != null) {
+            System.out.print(current.val);
+            if (current.next!= null) {
+                System.out.print("->");
+            }
+            current = current.next;
+        }
+    }
+    public static void main(String args[]) {
+        MergeKSortedLists obj = new MergeKSortedLists();
+        // [[1,4,5],[1,3,4],[2,6]]
+        ListNode list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode list3 = new ListNode(2, new ListNode(6));
+
+        ListNode res = obj.mergeKLists(new ListNode[]{list1, list2, list3});
+        obj.printLinkedList(res);
+    }
 }
